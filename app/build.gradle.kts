39e6767804e98f825432bcbf606f3a91b7f3f6a4
SIVARAMAN R <sivaram311@gmail.com>
2026-08-13 09:01:44 +0530

    Bump version to 0.2.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "buzz.delena.monolaunch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures {
